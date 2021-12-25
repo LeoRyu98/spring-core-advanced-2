7d57e2a81fdf411ea7a051b6523bd96fb3354ca2
kyeongwoo <drd9811@naver.com>
2021-12-25 18:26:56 +0900

    update 211225

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.cofig.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.cofig.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.cofig.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.cofig.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.cofig.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.cofig.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class}) // 해당 클래스를 스프링 빈으로 등록한다.
 //@Import(InterfaceProxyConfig.class)
 //@Import({ConcreteProxyConfig.class})
-@Import({DynamicProxyFilterConfig.class})
+//@Import({DynamicProxyFilterConfig.class})
+@Import({ProxyFactoryConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //hello.proxy.app package 만 컴포넌트 스캔의 대상이 된다.
 // Config 파일을 따로 @Import 를 통해서 스프링 빈으로 등록한 이유는 계속 다른 예제를 만들면서 Config 파일을 바꿔서 등록하기 위함
 public class ProxyApplication {
---
+++ src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,76 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
+public class MultiAdvisorTest {
+    
+    @Test
+    @DisplayName("여러 프록시")
+    public void multiAdvisorTest1() throws Exception {
+        // client -> proxy2(advisor2) -> proxy1(advisor1) -> target
+        // Pointcut.TRUE 설정으로 해당 테스트에서는 필터링을 하지 않음
+
+        // 프록시1 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        // 프록시2 생성 target -> proxy1 입력
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        proxyFactory2.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        // 실행
+        proxy2.save();
+    }
+
+    @Test
+    @DisplayName("하나의 프록시, 여러 어드바이저")
+    public void multiAdvisorTest2() throws Exception {
+        // client -> proxy -> advisor2 -> advisor1 -> target
+        // Pointcut.TRUE 설정으로 해당 테스트에서는 필터링을 하지 않음
+
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        // 프록시 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+
+        proxyFactory1.addAdvisor(advisor1);
+        proxyFactory1.addAdvisor(advisor2);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory1.getProxy();
+
+        // 실행
+        proxy.save();
+    }
+
+    static class Advice1 implements MethodInterceptor{
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    static class Advice2 implements MethodInterceptor{
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+}
